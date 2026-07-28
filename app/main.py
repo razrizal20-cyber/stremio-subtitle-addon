788187fc3981dfbf67814c6133554e1ba14395fa
razrizal20-cyber <razrizal20@gmail.com>
2026-07-28 16:57:44 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,28 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI(title="Rizal Subtitle Addon")
+from .telegram_bot import application
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await application.initialize()
+    await application.start()
+    await application.updater.start_polling()
+
+    yield
+
+    await application.updater.stop()
+    await application.stop()
+    await application.shutdown()
+
+
+app = FastAPI(
+    title="Rizal Subtitle Addon",
+    lifespan=lifespan
+)
+
 
 @app.get("/")
 async def home():
