57050738475dd974546def9522ea8e02b99aaa9a
razrizal20-cyber <razrizal20@gmail.com>
2026-07-28 23:15:22 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,18 +5,47 @@ from fastapi import FastAPI
 from .telegram_bot import application
 
 
+# ==========================
+# LIFESPAN
+# ==========================
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+
+    # buang webhook/polling lama
+    await application.bot.delete_webhook(
+        drop_pending_updates=True
+    )
+
+
+    # hidupkan telegram bot
     await application.initialize()
+
     await application.start()
-    await application.updater.start_polling()
+
+    await application.updater.start_polling(
+        drop_pending_updates=True
+    )
+
+
+    print("Telegram Bot Started")
+
 
     yield
 
+
+    # tutup bot dengan betul
     await application.updater.stop()
+
     await application.stop()
+
     await application.shutdown()
 
+
+
+# ==========================
+# FASTAPI
+# ==========================
 
 app = FastAPI(
     title="Rizal Subtitle Addon",
@@ -24,8 +53,11 @@ app = FastAPI(
 )
 
 
+
 @app.get("/")
 async def home():
+
     return {
-        "status": "online"
+        "status": "online",
+        "service": "Rizal Subtitle Addon"
     }
