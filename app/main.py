959fcb1dbb3f944ce1627c20a71024fc56d296a9
razrizal20-cyber <razrizal20@gmail.com>
2026-07-29 15:57:49 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -74,6 +74,50 @@ init_database()
 # HOME TEST
 # =========================
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    # buang webhook/polling lama
+    await application.bot.delete_webhook(
+        drop_pending_updates=True
+    )
+
+
+    # hidupkan telegram bot
+    await application.initialize()
+
+    await application.start()
+
+    await application.updater.start_polling(
+        drop_pending_updates=True
+    )
+
+
+    print("Telegram Bot Started")
+
+
+    yield
+
+
+    # tutup bot dengan betul
+    await application.updater.stop()
+
+    await application.stop()
+
+    await application.shutdown()
+
+
+
+# ==========================
+# FASTAPI
+# ==========================
+
+app = FastAPI(
+    title="Rizal Subtitle Addon",
+    lifespan=lifespan
+)
+
 @app.get("/")
 def home():
 
@@ -422,58 +466,4 @@ def subtitle_file(
 
         "File not found"
 
-    }
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-
-    # buang webhook/polling lama
-    await application.bot.delete_webhook(
-        drop_pending_updates=True
-    )
-
-
-    # hidupkan telegram bot
-    await application.initialize()
-
-    await application.start()
-
-    await application.updater.start_polling(
-        drop_pending_updates=True
-    )
-
-
-    print("Telegram Bot Started")
-
-
-    yield
-
-
-    # tutup bot dengan betul
-    await application.updater.stop()
-
-    await application.stop()
-
-    await application.shutdown()
-
-
-
-# ==========================
-# FASTAPI
-# ==========================
-
-app = FastAPI(
-    title="Rizal Subtitle Addon",
-    lifespan=lifespan
-)
-
-
-
-@app.get("/")
-async def home():
-
-    return {
-        "status": "online",
-        "service": "Rizal Subtitle Addon"
     }
